38ea7c4e6f7d7a91133610464f21c19efc489e03
rhobtor <luismidp7@gmail.com>
2024-04-23 09:52:32 +0200

    update code

---
+++ code/talker.py
@@ -0,0 +1,41 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class Talker(Node):
+    def __init__(self):
+        super().__init__('talker')
+        self.publisher_ = self.create_publisher(String, 'chatter', 10)
+        self.timer = self.create_timer(0.5, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+class Listener(Node):
+    def __init__(self):
+        super().__init__('listener')
+        self.subscription = self.create_subscription(String, 'chatter', self.listener_callback, 10)
+        self.subscription
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    talker = Talker()
+    listener = Listener()
+
+    rclpy.spin(listener)
+
+    talker.destroy_node()
+    listener.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
